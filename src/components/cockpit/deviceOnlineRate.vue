5085dc7403e33731e006bfe63b12d91b104ad4e1
黄达钊 <553775889@qq.com>
2024-03-13 15:00:41 +0800

    更改环形图适配

--- src/components/cockpit/deviceOnlineRate.vue
+++ src/components/cockpit/deviceOnlineRate.vue
@@ -2,6 +2,8 @@
   <div class="deviceOnlineRate-container">
     <dv-decoration-7 class="module-title">设备在线率</dv-decoration-7>
     <div class="ring-box">
+      <!-- <dv-water-level-pond :config="config" class="dv-ring" /> -->
+      <!-- <dv-charts class="dv-ring" :option="option" /> -->
       <dv-active-ring-chart class="dv-ring" :config="deviceOnlineData" />
     </div>
   </div>
@@ -10,7 +12,36 @@
 <script>
 export default {
   name: "deviceOnlineRate",
-  props: ["deviceOnlineData"]
+  props: ["deviceOnlineData"],
+  data() {
+    return {
+      option: {
+        series: [
+          {
+            type: "pie",
+            data: [
+              { name: "在线", value: 93 },
+              { name: "离线", value: 32 },
+              { name: "总数", value: 52 }
+            ],
+            radius: ["45%", "65%"],
+            insideLabel: {
+              show: false
+            },
+            outsideLabel: {
+              labelLineEndLength: 10,
+              formatter: "{percent}%\n{name}",
+              style: {
+                fontSize: 14,
+                fill: "#fff"
+              }
+            }
+          }
+        ],
+        color: ["#00baff", "#3de7c9", "#fff", "#ffc530", "#469f4b"]
+      }
+    };
+  }
 };
 </script>
 
@@ -18,8 +49,8 @@ export default {
 .deviceOnlineRate-container {
   width: 100%;
   height: 30%;
-  display: flex;
-  flex-direction: column;
+  /* display: flex; */
+  /* flex-direction: column; */
 }
 .ring-box {
   position: relative;
@@ -32,8 +63,8 @@ export default {
   /* position: relative; */
 }
 .dv-ring {
-  width: 100%;
-  height: 100%;
+  width: 160px;
+  height: 160px;
   /* position: absolute;
   width: auto;
   max-height: 100%;
