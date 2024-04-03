d24eced9f927b0ec9f342fda39fc203154b97cab
wuyong <yongwangyi999@163.com>
2024-04-03 13:29:51 +0800

    修复webrtc无法正常播放问题

--- config/index.js
+++ config/index.js
@@ -12,8 +12,8 @@ module.exports = {
     proxyTable: {
       "/debug": {
         // target: 'http://localhost:18080',
-        target: 'http://172.16.1.106:18080',
-        // target: 'http://192.168.1.105:18080',
+        // target: 'http://172.16.1.106:18080',
+        target: 'http://192.168.1.105:18080',
         changeOrigin: true,
         pathRewrite: {
           "^/debug": "/"
@@ -21,8 +21,8 @@ module.exports = {
       },
       "/static/snap": {
         // target: 'http://localhost:18080',
-        target: 'http://172.16.1.106:18080',
-        // target: 'http://192.168.1.105:18080',
+        // target: 'http://172.16.1.106:18080',
+        target: 'http://192.168.1.105:18080',
         changeOrigin: true,
         // pathRewrite: {
         //   '^/static/snap': '/static/snap'
@@ -30,8 +30,9 @@ module.exports = {
       },
       "/debug/video": {
         // target: 'http://localhost:18080',
-        target: "http://183.239.58.24:18080/ai/api",
-        // target: 'http://192.168.1.110:18080',
+        // target: "http://183.239.58.24:18080/ai/api",
+        target: "http://192.168.1.105:18080/ai/api",
+        // target: 'http://192.168.1.105:18080',
         changeOrigin: true,
         pathRewrite: {
           "^/debug/video": ""
@@ -40,11 +41,11 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host:"172.16.1.106",
-    // host:"192.168.1.105",
+    // host:"172.16.1.106",
+    host:"192.168.1.105",
     useLocalIp: false, // can be overwritten by process.env.HOST
-    // port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
