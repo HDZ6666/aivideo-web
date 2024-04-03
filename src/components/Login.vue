8b30c3a298a60ca8c269f4381895bc621955a41d
wuyong <yongwangyi999@163.com>
2024-04-03 12:36:14 +0800

    优化登录页，允许域名访问

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,7 @@
   <div class="limiter">
     <div class="container-login100">
       <div class="wrap-login100">
-					<span class="login100-form-title p-b-26">佛山移动AI+视频管理平台</span>
+					<span class="login100-form-title p-b-26">视频+AI管理平台</span>
           <span class="login100-form-title p-b-48">
 						<i class="fa fa-video-camera"></i>
 					</span>
