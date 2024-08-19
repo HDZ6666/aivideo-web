f42cfa8a2117e60884971284848baff07455bfc0
黄达钊 <553775889@qq.com>
2024-08-19 14:43:16 +0800

    feat：恢复一楼配置

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -4,8 +4,8 @@ export const mixin = {
       alarmNotify: !localStorage.getItem("alarmSwitchStatus")
         ? false
         : JSON.parse(localStorage.getItem("alarmSwitchStatus")),
-      playerType: "jessibuca", // liveplayer | jessibuca
-      playerAction: "national", // national (国标) | proxy (拉流) | nationalCockpit (国标无层级)
+      playerType: "liveplayer", // liveplayer | jessibuca
+      playerAction: "proxy", // national (国标) | proxy (拉流) | nationalCockpit (国标无层级)
       aiType: "nationalAI" // proxyAI（后端分析） | nationalAI （设备自带AI） | false (没有AI)
     };
   }
