2c866087bc21c6a0e0106fc7c92c80a28c7189be
moweilin <moweilin@live.com>
2024-09-24 18:13:54 +0800

    调整代码布局

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# AiVideoWeb
+## 开发说明
+|目录|名称|
+|:-:|:-:|
+|aivideo-web-v1|旧项目|
+|aivideo-web-v2|新项目|
 
-基于GB28181-2016标准的智能视频平台前端，负责实现核心信令与设备管理后台部分，支持NAT穿透，支持海康、大华、宇视等品牌的IPC、NVR接入。支持国标级联，也支持不带国标功能的摄像机/直播流/直播推流转发到其他国标视频平台。
+后续新功能尽量在aivideo-web-v2上开发
+### aivideo-web.code-workspace
+配置了vscode的工作空间
+### aivideo-web-v1
+aivideo-web-v1的登录页有v2链接，并将以/v2开头的请求代理到aivideo-web-v2
+- 开发
+```
+npm run dev
+```
+### aivideo-web-v2
+aivideo-web-v2使用[td-starter](https://tdesign.tencent.com/starter/docs/vue-next/get-started)创建
+
+```
+pnpm dev或npm run dev
+```
---
+++ aivideo-web-v1/config/index.js
@@ -0,0 +1,106 @@
+"use strict";
+// Template version: 1.3.1
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require("path");
+
+module.exports = {
+  dev: {
+    // Paths
+    assetsSubDirectory: "static",
+    assetsPublicPath: "/",
+    proxyTable: {
+      "/debug": {
+        // target: 'http://localhost:18080',
+        // target: 'http://172.16.1.106:18080',
+        // target: "http://192.168.1.105:18080",
+        // target: "http://10.16.139.254:18080",
+        // target: "https://fyict.cn:18080",
+        target: "http://183.239.58.24:18080",
+        // target: "http://192.168.0.151:18080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/debug": "/"
+        }
+      },
+      "/static/snap": {
+        // target: 'http://localhost:18080',
+        // target: "http://172.16.1.106:18080",
+        // target: "http://192.168.1.105:18080",
+        target: "http://183.239.58.24:18080",
+        changeOrigin: true
+        // pathRewrite: {
+        //   '^/static/snap': '/static/snap'
+        // }
+      },
+      "/v2":{
+        target: "http://localhost:5173",
+        changeOrigin: true
+      }
+    },
+
+    // Various Dev Server settings
+    // host:"172.16.1.106",
+    host: "127.0.0.1",
+    useLocalIp: false, // can be overwritten by process.env.HOST
+    // port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    hot: true, //自动保存
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: "cheap-module-eval-source-map",
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    cssSourceMap: true
+  },
+
+  build: {
+    // Template for index.html
+    // index: path.resolve(
+    //   __dirname,
+    //   "../../src/main/resources/static/index.html"
+    // ),
+
+    index: path.resolve(__dirname, "../dist/index.html"),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, "../dist/"),
+    // assetsRoot:  path.resolve(__dirname, "../../src/main/resources/static/"),
+    // assetsRoot:'dist',
+    assetsSubDirectory: "./static",
+    assetsPublicPath: "/",
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: "#source-map",
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ["js", "css"],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
+};
