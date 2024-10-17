0e7e723478d80d9c48519d631bfdf866a57f44cf
xiaoC <chenlang@gd.chinamobile.com>
2024-10-17 11:13:07 +0800

    本次提交优化了巡逻管理和巡逻报告前端界面

--- src/components/dialog/showReport.vue
+++ src/components/dialog/showReport.vue
@@ -42,7 +42,11 @@
                 <el-table-column prop="camerasName" label="摄像头名称" width="180"></el-table-column>  
                 <el-table-column label="巡逻拍照图片">  
                   <template slot-scope="scope">  
-                    <img :src="scope.row.imageUrl" style="width: 100px; height: 75px;" />  
+                    <img 
+                      :src="scope.row.imageUrl" 
+                      style="width: 100px; height: 75px; cursor: pointer;"
+                      @click="handleClick(scope.row.imageUrl)"  
+                       />  
                   </template>  
                 </el-table-column>  
                 <el-table-column label="是否有告警">  
@@ -64,10 +68,20 @@
                   </template>  
                 </el-table-column>  
               </el-table>  
-            </el-form-item>  
+            </el-form-item> 
           </el-col>  
-        </el-row>  
+        </el-row> 
       </el-form>  
+    </el-dialog> 
+    
+    <el-dialog  
+      title="查看图片"  
+      :visible.sync="showImageDialogVisible"  
+      width="80%" 
+      top="5vh"  
+      :before-close="handleImageClose"  
+    >  
+      <img :src="imageSrc" style="width: 100%; height: 100%;margin-top: -20px;" />  
     </el-dialog>  
   </div>  
 </template>  
@@ -91,11 +105,23 @@ export default {
     handleClose() {  
       this.dialogVisible = false;  
     },  
+
+    // 点击图片时打开查看图片对话框  
+    handleClick(imageUrl) {  
+      this.imageSrc = imageUrl;  
+      this.showImageDialogVisible = true;  
+    },  
+    // 关闭查看图片对话框  
+    handleImageClose() {  
+      this.showImageDialogVisible = false;  
+    },  
   },  
   data() {  
     return {  
-      dialogVisible: false,  
-      formLabelWidth: '120px',  
+      dialogVisible: false,  // 是否显示报告对话框  
+      formLabelWidth: '120px',
+      showImageDialogVisible: false,  // 是否显示查看图片对话框  
+      imageSrc: '',  // 查看图片的URL  
       reportForm: {  
         reportID: '',  
         routename: '',  
@@ -104,13 +130,13 @@ export default {
       },  
       originalReport: {}, // 用于存储原始数据，以便在取消时恢复  
       reportList: [  
-        { reportID: 1, routename: '路线1', camerasName: '摄像头1', imageUrl: '', hasAlert: false, alertType: '', alertName: '' },  
-        { reportID: 1, routename: '路线1', camerasName: '摄像头2', imageUrl: '', hasAlert: true, alertType: '人体', alertName: '人体检测告警' },  
-        { reportID: 1, routename: '路线1', camerasName: '摄像头2', imageUrl: '', hasAlert: true, alertType: '环境', alertName: '环境异常告警' },  
-        { reportID: 2, routename: '路线2', camerasName: '摄像头3', imageUrl: '', hasAlert: true, alertType: '人体', alertName: '人体检测告警' },  
-        { reportID: 2, routename: '路线2', camerasName: '摄像头4', imageUrl: '', hasAlert: true, alertType: '环境', alertName: '环境异常告警' },  
-        { reportID: 3, routename: '路线3', camerasName: '摄像头5', imageUrl: '', hasAlert: false, alertType: '', alertName: '' },  
-        { reportID: 3, routename: '路线3', camerasName: '摄像头6', imageUrl: '', hasAlert: false, alertType: '', alertName: '' },  
+        { reportID: 1, routename: '路线1', camerasName: '摄像头1', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707165058-e633b322792f10b237869e9320a6ee67.jpeg', hasAlert: false, alertType: '', alertName: '' },  
+        { reportID: 1, routename: '路线1', camerasName: '摄像头2', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707161201-ac69968c5c6e5082eb60de5f83a307fe.jpeg', hasAlert: true, alertType: '人体', alertName: '人体检测告警' },  
+        { reportID: 1, routename: '路线1', camerasName: '摄像头2', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240705/photo-gathering-20240705193811-f2c6f38066fdcaed202226dde57a27ed.jpeg', hasAlert: true, alertType: '环境', alertName: '环境异常告警' },  
+        { reportID: 2, routename: '路线2', camerasName: '摄像头3', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707153308-46cbf264ad75820f44bb7999d3037d96.jpeg', hasAlert: true, alertType: '人体', alertName: '人体检测告警' },  
+        { reportID: 2, routename: '路线2', camerasName: '摄像头4', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707151542-7c2c8af40c96b71fb6f86dbc2c4ba0d7.jpeg', hasAlert: true, alertType: '环境', alertName: '环境异常告警' },  
+        { reportID: 3, routename: '路线3', camerasName: '摄像头5', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707143138-63a1045ab1cf7f82b8b5b2745b82f553.jpeg', hasAlert: false, alertType: '', alertName: '' },  
+        { reportID: 3, routename: '路线3', camerasName: '摄像头6', imageUrl: 'https://ai-monitor-qy.eos-dongguan-8.cmecloud.cn/1006/20240707/photo-gathering-20240707134437-753ab7d12f34ef3a4e201da81d83c0f5.jpeg', hasAlert: false, alertType: '', alertName: '' },  
       ],  
       filteredReportList: [], // 用于存储筛选后的报告列表  
     };  
