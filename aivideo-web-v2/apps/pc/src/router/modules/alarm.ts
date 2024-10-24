d669698fd85a3619189460b659a3062f39fe6627
tutu_fems <13922031500@139.com>
2024-10-24 18:48:29 +0800

    通知
    Signed-off-by: tutu_fems <13922031500@139.com>

--- aivideo-web-v2/apps/pc/src/router/modules/alarm.ts
+++ aivideo-web-v2/apps/pc/src/router/modules/alarm.ts
@@ -11,21 +11,21 @@ export default [
     children: [
       {
         path: 'rules/index',
-        name: 'AlarmRules',
+        name: 'Rules/Index',
         component: () => import('@/pages/alarm/rules/index.vue'),
         meta: { title: { zh_CN: '内置规则', en_US: 'Alarm Center' } },
       },
       {
-        path: 'index1',
-        name: 'AlarmIndex',
-        component: () => import('@/pages/user/index.vue'),
-        meta: { title: { zh_CN: '告警中心', en_US: 'Alarm Center' } },
+        path: 'notifierTargets/index',
+        name: 'NotifierTargetsIndex',
+        component: () => import('@/pages/alarm/notifierTargets/index.vue'),
+        meta: { title: { zh_CN: '通知目标', en_US: 'Alarm Center' } },
       },
       {
-        path: 'index1',
-        name: 'AlarmIndex',
-        component: () => import('@/pages/user/index.vue'),
-        meta: { title: { zh_CN: '告警中心', en_US: 'Alarm Center' } },
+        path: 'notifications/index',
+        name: 'NotificationsIndex',
+        component: () => import('@/pages/alarm/notifications/index.vue'),
+        meta: { title: { zh_CN: '通知', en_US: 'Alarm Center' } },
       },
       {
         path: 'index1',
