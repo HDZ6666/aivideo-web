b8bf410e710d0a2b4a09c20d59d797bb983284a8
18218600404 <18218600404@139.com>
2024-10-24 08:42:02 +0800

    添加大屏页面

--- aivideo-web-v2/apps/datav/src/main.ts
+++ aivideo-web-v2/apps/datav/src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
-import './style.css'
+import TDesign from 'tdesign-vue-next'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import http from './api/http'
+import base from './assets/script/common'
+import './style.css'
+import 'tdesign-vue-next/es/style/index.css';
 
-createApp(App).mount('#app')
+import * as echarts from 'echarts';
+import 'echarts-gl'
+
+const app = createApp(App);
+app.config.globalProperties.$base = base;
+app.config.globalProperties.$http = http;
+app.config.globalProperties.$echart = echarts;
+app.use(store)
+    .use(router)
+    .use(TDesign)
+    .mount('#app');
