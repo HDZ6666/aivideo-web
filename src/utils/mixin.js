90fcd3bc5c61d884bd6a985217e062434c3860a2
黄达钊 <553775889@qq.com>
2024-11-11 14:51:58 +0800

    test: 测试合并远程仓库

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -6,7 +6,7 @@ export const mixin = {
         : JSON.parse(localStorage.getItem("alarmSwitchStatus")),
       playerType: "jessibuca", // liveplayer | jessibuca
       playerAction: "national", // national (国标) | proxy (拉流) | nationalCockpit (国标无层级)
-      aiType: "proxyAI" // proxyAI（后端分析） | nationalAI （设备自带AI） | false (没有AI)
+      aiType: "nationalAI" // proxyAI（后端分析） | nationalAI （设备自带AI） | false (没有AI)
     };
   }
 };
